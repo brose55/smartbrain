ae7cf277b872dffac11448d3ad7d930c4864e208
Brandon Rose <brandonrose805@gmail.com>
2020-10-17 21:43:48 -0700

    Updates

---
+++ precache-manifest.814ff9b9ae083046334e012621e79fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01f024ce7cdc791020def1ff7fbe2aa",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "a2ce3c3e3757893e94c6",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "63a59d321ae74ca183cb",
+    "url": "/smart-brain/static/css/main.b33a343b.chunk.css"
+  },
+  {
+    "revision": "a2ce3c3e3757893e94c6",
+    "url": "/smart-brain/static/js/2.134df75c.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/smart-brain/static/js/2.134df75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a59d321ae74ca183cb",
+    "url": "/smart-brain/static/js/main.ec1e2419.chunk.js"
+  },
+  {
+    "revision": "ff05128447b9a9e9960b",
+    "url": "/smart-brain/static/js/runtime-main.ec5df7e1.js"
+  }
+]);
